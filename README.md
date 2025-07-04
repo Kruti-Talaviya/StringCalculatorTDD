b7c1cc1bedf0a6c2b688542e4180d84f128b247c
unknown <talaviyakruti11@gmail.com>
2025-07-04 21:56:49 +0530

    test case when string is empty

---
+++ README.md
@@ -0,0 +1,3 @@
+# StringCalculatorTDD
+# StringCalculatorTDD
+# StringCalculatorTDD
---
+++ src/stringCalculator.test.js
@@ -0,0 +1,6 @@
+
+const { add } = require('./stringCalculator');
+
+test('returns 0 for empty string', () => {
+  expect(add("")).toBe(0);
+});
