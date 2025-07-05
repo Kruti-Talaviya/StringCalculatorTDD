396b3304c990ccdc8f498a3c8420c02e55c81f20
unknown <talaviyakruti11@gmail.com>
2025-07-05 09:35:25 +0530

    returns sum of any comma-separated numbers

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -10,11 +10,15 @@ describe('add',()=>{
   it('return number it self if one number is given',()=>{
     expect(add("1")).toBe(1);
   });
-  
+
   it('returns sum of two comma-separated numbers', () => {
   expect(add("1,2")).toBe(3);
   });
   
+  it('return sum of any comma-seprated numbers',()=>{
+    expect(add("1,2,3")).toBe(6);
+  });
+
 })
 
 
