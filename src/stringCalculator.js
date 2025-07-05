da5c091703f98951ef1806d85f02100d5e3946fc
unknown <talaviyakruti11@gmail.com>
2025-07-05 09:29:48 +0530

    returns sum of two comma-separated numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,8 @@
 
 function add(numbers) {
     if (numbers === "") return 0;
-    if(!numbers.includes(",")) return parseInt(numbers);
+    const parts = numbers.split(",");
+    return parts.reduce((sum,num)=>sum + parseInt(num),0);
   }
   
   module.exports = { add };
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -2,12 +2,18 @@
 const { add } = require('./stringCalculator');
 
 describe('add',()=>{
+
   it('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
   });
+
   it('return number it self if one number is given',()=>{
     expect(add("1")).toBe(1);
-  })
+  });
+  
+  it('returns sum of two comma-separated numbers', () => {
+  expect(add("1,2")).toBe(3);
+  });
   
 })
 
